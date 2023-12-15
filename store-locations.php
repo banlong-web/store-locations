d048e2c2f24303dc3897fabae315fc5da6973197
BanLong <banlongtc@gmail.com>
2023-12-15 10:42:54 +0700

    first commit

---
+++ store-locations.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Tìm kiếm địa điểm cửa hàng
+ * Description: Tìm kiếm các cửa hàng với sản phẩm còn hàng trong cửa hàng.
+ * Plugin URI: http://wordpress.org/plugins/store-locations/
+ * Author: 5ChauTeamDev
+ * Version: 1.0.0
+ */
